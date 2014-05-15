6d13b164dfd9eacffa77925dd2606342697aa415
tcaffelle032 <tcaffelle@gmail.com>
2014-05-15 15:59:44 -0400

    Added basic Player
    added basic player with movement based on W,A,,S,D

---
+++ LunarFlare/Code/Assets/Cus Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public int playerSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		GameObject player = GameObject.Find("Player");
+		 
+		if(Input.anyKey){
+			playerMovement(player);
+		}
+		else if(!Input.anyKey) {
+			player.rigidbody.velocity = Vector3.zero;
+		}
+
+
+			player.transform.rotation =  Quaternion.identity;
+
+
+	
+	}
+
+	void playerMovement(GameObject player){
+
+
+		if(Input.GetKeyDown(KeyCode.W)){
+			player.rigidbody.velocity = player.transform.forward * playerSpeed;			
+		}
+
+		else if(Input.GetKeyDown(KeyCode.S)){
+			player.rigidbody.velocity = -player.transform.forward * playerSpeed;		
+		}
+		else if(Input.GetKeyDown(KeyCode.D)){
+			player.rigidbody.velocity = player.transform.right * playerSpeed;	
+		}
+		else if(Input.GetKeyDown(KeyCode.A)){
+			player.rigidbody.velocity = -player.transform.right * playerSpeed;	
+		}
+	
+
+
+	}
+}
