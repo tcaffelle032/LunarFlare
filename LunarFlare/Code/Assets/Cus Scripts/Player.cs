fe187d4cd1fb79052d39ad39933cdea4447073b2
tcaffelle032 <tcaffelle@gmail.com>
2014-05-21 15:56:06 -0400

    Added Sprite, Removed package
    Added a sprite with animations, removed a package we did not need.

--- LunarFlare/Code/Assets/Cus Scripts/Player.cs
+++ LunarFlare/Code/Assets/Cus Scripts/Player.cs
@@ -17,9 +17,7 @@ public class Player : MonoBehaviour {
 		}
 		else if(!Input.anyKey) {
 			player.rigidbody.velocity = new Vector3(0,Physics.gravity.y,0);
-			Animator animate = this.GetComponent<Animator>();
-			AnimatorStateInfo stateInfo = animate.GetCurrentAnimatorStateInfo(0);
-			animate.SetBool("Walk_Down",false);
+			playerStopped(false);
 		}
 		player.transform.rotation =  Quaternion.identity;
 	}
@@ -27,8 +25,10 @@ public class Player : MonoBehaviour {
 	void playerMovement(GameObject player){
 		Animator animate = this.GetComponent<Animator>();
 		AnimatorStateInfo stateInfo = animate.GetCurrentAnimatorStateInfo(0);
+
 		if(Input.GetKeyDown(KeyCode.W)){
-			player.rigidbody.velocity = player.transform.forward * playerSpeed;			
+			player.rigidbody.velocity = player.transform.forward * playerSpeed;		
+			animate.SetBool("Walk_Up",true);
 		}
 
 		else if(Input.GetKeyDown(KeyCode.S)){
@@ -37,13 +37,23 @@ public class Player : MonoBehaviour {
 		}
 		else if(Input.GetKeyDown(KeyCode.D)){
 			player.rigidbody.velocity = player.transform.right * playerSpeed;	
+			animate.SetBool("Walk_Right",true);
 
 		}
 		else if(Input.GetKeyDown(KeyCode.A)){
-			player.rigidbody.velocity = -player.transform.right * playerSpeed;	
+			player.rigidbody.velocity = -player.transform.right * playerSpeed;
+			player.transform.rotation = new Quaternion(0,-1,0,0);
+			animate.SetBool("Walk_Left",true);
 		}
-	
-
-
 	}
+	void playerStopped(bool isIdle){
+		Animator animate = this.GetComponent<Animator>();
+		AnimatorStateInfo stateInfo = animate.GetCurrentAnimatorStateInfo(0);
+
+		animate.SetBool("Walk_Down",false);
+		animate.SetBool("Walk_Right",false);
+		animate.SetBool("Walk_Left",false);
+		animate.SetBool("Walk_Up",false);
+	}
+
 }
