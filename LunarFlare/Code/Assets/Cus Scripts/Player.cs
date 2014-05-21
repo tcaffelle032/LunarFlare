ae0994faa80a02113cf17c98e33718baaec0593b
tcaffelle032 <tcaffelle@gmail.com>
2014-05-21 14:23:56 -0400

    Animation
    Some crappy animation. Physics broke though.

--- LunarFlare/Code/Assets/Cus Scripts/Player.cs
+++ LunarFlare/Code/Assets/Cus Scripts/Player.cs
@@ -17,27 +17,27 @@ public class Player : MonoBehaviour {
 		}
 		else if(!Input.anyKey) {
 			player.rigidbody.velocity = new Vector3(0,Physics.gravity.y,0);
+			Animator animate = this.GetComponent<Animator>();
+			AnimatorStateInfo stateInfo = animate.GetCurrentAnimatorStateInfo(0);
+			animate.SetBool("Walk_Down",false);
 		}
-
-
-			player.transform.rotation =  Quaternion.identity;
-
-
-	
+		player.transform.rotation =  Quaternion.identity;
 	}
 
 	void playerMovement(GameObject player){
-
-
+		Animator animate = this.GetComponent<Animator>();
+		AnimatorStateInfo stateInfo = animate.GetCurrentAnimatorStateInfo(0);
 		if(Input.GetKeyDown(KeyCode.W)){
 			player.rigidbody.velocity = player.transform.forward * playerSpeed;			
 		}
 
 		else if(Input.GetKeyDown(KeyCode.S)){
-			player.rigidbody.velocity = -player.transform.forward * playerSpeed;		
+			player.rigidbody.velocity = -player.transform.forward * playerSpeed;
+			animate.SetBool("Walk_Down",true);
 		}
 		else if(Input.GetKeyDown(KeyCode.D)){
 			player.rigidbody.velocity = player.transform.right * playerSpeed;	
+
 		}
 		else if(Input.GetKeyDown(KeyCode.A)){
 			player.rigidbody.velocity = -player.transform.right * playerSpeed;	
