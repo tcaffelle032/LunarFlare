069609834eab597a2b32f361e8ab535787aaa465
tcaffelle032 <tcaffelle@gmail.com>
2014-05-29 16:35:33 -0400

    NPC Creator
    Created basic NPC Creator

---
+++ LunarFlare/Code/Assets/Cus Scripts/NPC_Spawner.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC_Spawner : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var npcNum = Random.Range(0,5);
+		createNPC(npcNum);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void createNPC(int count){
+		for(int i = 0; i <= count; i++){
+			float rand = Random.Range(-10,10);
+			GameObject.Instantiate(Resources.Load("NPC"),new Vector3(rand,3,rand),Quaternion.identity);
+		}
+	}
+}
