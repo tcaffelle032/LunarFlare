359ae7eae2acbfe652e78c8978e8dc04d2fe9492
tcaffelle032 <tcaffelle@gmail.com>
2014-05-29 14:05:49 -0400

    Added NPC
    Basic NPC added, just moves around randomly

---
+++ LunarFlare/Code/Assets/Cus Scripts/N_NPC.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class N_NPC : MonoBehaviour {
+	float timer = 3;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timer -= Time.deltaTime;
+		//Debug.Log(timer);
+		if(timer <= 0)
+			NPCMovement(Random.Range(0,5),3);
+		var npc = this.gameObject;
+		npc.transform.rotation = Quaternion.identity;
+	}
+
+	void NPCMovement(int direction,int speed){
+		var npc = this.gameObject;
+		timer = 3;
+
+		switch(direction){
+		case 1:
+			npc.rigidbody.velocity = npc.transform.forward * speed;
+			break;
+		case 2:
+			npc.rigidbody.velocity = -npc.transform.forward * speed;
+			break;
+		case 3:
+			npc.rigidbody.velocity = npc.transform.right * speed;
+			break;
+		case 4:
+			npc.rigidbody.velocity = -npc.transform.right * speed;
+			break;
+		 default:
+			npc.rigidbody.velocity = npc.transform.forward * 0;
+			break;
+
+		}
+	}
+}
